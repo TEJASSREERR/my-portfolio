00108eaef667fa446971d58a93a5b1ebeeac5a39
Tejassree <tejassreerr@gmail.com>
2026-06-20 19:32:01 +0530

    skills changes

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,10 +1,14 @@
 export const skills = [
+  { name: 'Python', level: 80 },
+  { name: 'C programming', level: 77 },
   { name: 'HTML', level: 85 },
   { name: 'CSS', level: 80 },
-  { name: 'JavaScript', level: 79 },
-  { name: 'React', level: 75 },
   { name: 'Tailwind CSS', level: 75 },
+  { name: 'JavaScript', level: 79 },
+  { name: 'Django', level: 75 },
+  { name: 'MongoDB', level: 74 },
+  { name: 'React.js', level: 75 },
+  { name: 'Next.js', level: 75 },
   { name: 'Git & GitHub', level: 77 },
-  { name: 'Python', level: 80 },
   { name: 'Node.js', level: 75 },
 ]
